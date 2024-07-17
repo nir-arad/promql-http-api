e50fad8b609f7a6a009a788b0428182cb02163d9
Nir Arad <narad@nvidia.com>
2024-07-17 12:08:47 -0700

    Add error checking to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 echo Updating poetry.lock
-poetry update
+poetry update || { echo "Failed to update poetry.lock"; exit 1; }
 
 echo update package installation
-pip install -e .
+pip install -e . || { echo "Failed to update package installation"; exit 1; }
 
 echo Running tests
-poetry run pytest
+poetry run pytest || { echo "Failed to run tests"; exit 1; }
 
 echo Building the package
-poetry build
+poetry build || { echo "Failed to build the package"; exit 1; }
 
 echo Checking the package
-poetry check
+poetry check || { echo "Failed to check the package"; exit 1; }
 
 echo Publishing the package to PyPI
-poetry publish
+poetry publish || { echo "Failed to publish the package to PyPI"; exit 1; }
